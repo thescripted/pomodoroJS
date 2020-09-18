7bb6a42fb95b604c35e946ccf261a41470fd1a09
Benjamin Kinga <somethingscripted@gmail.com>
2020-09-17 21:38:19 -0500

    added project

---
+++ script.js
@@ -0,0 +1,102 @@
+const INITIAL_TIME = 1500; // 1500 seconds = 25 minutes
+const SECONDS = 1000; // Develop purposes only. 1000 ms
+
+function formatToTimeString(timeSeconds) {
+  let minutes = Math.floor(timeSeconds / 60);
+  let seconds = timeSeconds % 60;
+
+  function padded(time) {
+    return '0' + time;
+  }
+
+  if (seconds < 10) {
+    seconds = padded(seconds);
+  }
+
+  if (minutes < 10) {
+    minutes = padded(minutes);
+  }
+
+  const formatted = minutes + ':' + seconds;
+  return formatted;
+}
+// Called by Timer to update the DOM
+function renderTime(time) {
+  formatTime = formatToTimeString(time);
+  timerView.innerHTML = formatTime;
+  document.title = formatTime;
+}
+
+class Timer {
+  ticker = undefined;
+  timeout = undefined;
+  clock = INITIAL_TIME;
+  running = false;
+
+  start() {
+    let self = this;
+    this.running = true;
+    this.ticker = setInterval(function () {
+      if (self.clock < 0) {
+        clearInterval(self.ticker);
+      }
+      self.clock = self.clock - 1;
+      renderTime(self.clock);
+    }, SECONDS);
+
+    this.timeout = setTimeout(function () {
+      clearInterval(self.ticker);
+    }, self.clock * SECONDS);
+  }
+
+  pause() {
+    this.running = false;
+    clearInterval(this.ticker);
+    clearTimeout(this.timeout);
+    this.ticker = undefined;
+    this.timeout = undefined;
+    renderTime(this.clock);
+  }
+
+  reset() {
+    this.pause();
+    this.clock = INITIAL_TIME;
+    renderTime(this.clock);
+  }
+}
+
+const TIMER_INSTANCE = new Timer(); // One and only instance
+const playPauseHandler = function () {
+  if (!TIMER_INSTANCE.running) {
+    TIMER_INSTANCE.start();
+    playButton.innerText = 'Pause';
+    body.classList.remove('pauseScreen');
+    body.classList.add('playScreen');
+  } else {
+    TIMER_INSTANCE.pause();
+    playButton.innerText = 'Play';
+    body.classList.add('pauseScreen');
+    body.classList.remove('playScreen');
+  }
+};
+const resetHandler = function () {
+  TIMER_INSTANCE.reset();
+  playButton.innerText = 'Play';
+  body.classList.add('pauseScreen');
+  body.classList.remove('playScreen');
+};
+function initialRender() {
+  formatTime = formatToTimeString(INITIAL_TIME);
+  timerView.innerText = formatTime;
+  playButton.innerText = 'Play';
+}
+
+// Render Phase
+const timerView = document.getElementById('timer'); // Updated by renderTime()
+const playButton = document.getElementById('playPause');
+const resetButton = document.getElementById('reset');
+const body = document.querySelector('.body');
+playButton.addEventListener('click', playPauseHandler);
+resetButton.addEventListener('click', resetHandler);
+
+initialRender(); // default set up
